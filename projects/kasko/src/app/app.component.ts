d7157d4b951610a76e156587c92e4a8b3adda77d
Maxim Matyunin <krabovm@gmail.com>
2021-06-03 19:04:35 +0400

    create test form

--- projects/kasko/src/app/app.component.ts
+++ projects/kasko/src/app/app.component.ts
@@ -1,32 +1,74 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, NgZone, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+
+interface Region {
+  id: string;
+  name: string;
+}
+
+const regions: Region[] = [
+  'Москва',
+  'Калифорния',
+  'Тибет',
+  'Тайвань',
+].map((name, index) => ({id: index.toString(), name}));
+
+enum PolicyType {
+  Other = 'other',
+  VSK = 'vsk',
+}
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'kasko';
+export class AppComponent implements OnInit {
+  readonly isLoadingRegions$ = new BehaviorSubject(true);
+  readonly regions$ = new BehaviorSubject<Region[]>([]);
+  readonly PolicyType = PolicyType;
+
+  /**
+   * Value is a region's id
+   */
+  readonly regionControl = new FormControl();
+  readonly alreadyHasPolicyControl = new FormControl(false);
+  readonly existingPolicyTypeControl = new FormControl(PolicyType.Other);
+  readonly isPolicyOpenControl = new FormControl(false);
+  readonly driversControl = new FormArray([]);
+
+  readonly form = new FormGroup({
+    region: this.regionControl,
+    alreadyHasPolicy: this.alreadyHasPolicyControl,
+    existingPolicyType: this.existingPolicyTypeControl,
+    isPolicyOpen: this.isPolicyOpenControl,
+    drivers: this.driversControl,
+  });
+
+  constructor(private readonly ngZone: NgZone) {}
+
+  ngOnInit() {
+    this.addDriver();
+    this.ngZone.runOutsideAngular(() => {
+      setTimeout(() => {
+        this.isLoadingRegions$.next(false);
+        this.regions$.next(regions);
+      }, 3000);
+    });
+  }
+
+  onSearch(query: string) {
+    this.regions$.next(regions.filter(r => r.name.toLowerCase().includes(query.toLowerCase())));
+  }
+
+  addDriver() {
+    const driverControl = new FormGroup({
+      birthDate: new FormControl(),
+      drivingStartDate: new FormControl(),
+      isMarried: new FormControl(false),
+    });
+    this.driversControl.push(driverControl);
+  }
 }
--- projects/kasko/src/app/app.module.ts
+++ projects/kasko/src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { ru_RU, NZ_I18N } from 'ng-zorro-antd/i18n';
 
 import { AppComponent } from './app.component';
+
+import { registerLocaleData } from '@angular/common';
+import ru from '@angular/common/locales/ru';
+registerLocaleData(ru);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzSelectModule,
+    NzRadioModule,
+    NzButtonModule,
+    NzInputModule,
+    NzCheckboxModule,
+    NzDatePickerModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: NZ_I18N,
+      useValue: ru_RU,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
